f18ceaf9ca921edfe9b754ba6d482317a13c7a69
Rahul Shaw <134435513+rahulshaw8@users.noreply.github.com>
2025-04-18 09:47:32 +0530

    Add files via upload

--- nodes/antientropy.go
+++ nodes/antientropy.go
@@ -1,1 +1,19 @@
+package nodes
 
+import (
+	"github.com/Arbaba/Peerster/packets"
+	"time"
+)
+
+func (gossiper *Gossiper) AntiEntropyLoop() {
+	ticker := time.NewTicker(time.Second * time.Duration(gossiper.AntiEntropy))
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ticker.C:
+			status := gossiper.GetStatusPacket()
+			gossiper.SendPacketRandom(packets.GossipPacket{StatusPacket: status})
+		}
+	}
+
+}
